5e38e75e84669fe2bc3c85db394c4153b0607ccc
sotto97 <sotto.bw97@gmail.com>
2021-03-16 23:31:55 +0900

    サイドバー・ヘッダーの文字を追加

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,79 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer app v-model="drawer" clipped>
+      <v-container>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              Navigation lists
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list dense nav>
+          <v-list-item v-for="nav_list in nav_lists" :key="nav_list.name">
+            <v-list-item-icon>
+              <v-icon>{{ nav_list.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ nav_list.name }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-container>
+    </v-navigation-drawer>
+    <v-app-bar color="primary" dark app clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer"> </v-app-bar-nav-icon>
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn text>For Enterprise</v-btn>
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on" text>Support<v-icon>mdi-menu-down</v-icon></v-btn>
+          </template>
+          <v-list>
+            <v-subheader>Get Help</v-subheader>
+            <v-list-item v-for="support in supports" :key="support.name">
+              <v-list-item-icon>
+                <v-icon>{{ support.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{ support.name }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-toolbar-items>
+      <!-- <v-btn outlined>Button</v-btn> -->
+    </v-app-bar>
+    <v-footer color="primary" dark app>
+      Vuetify
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      drawer: null,
+      supports: [
+        { name: 'Consulting and suppourt', icon: 'mdi-vuetify' },
+        { name: 'Discord community', icon: 'mdi-discord' },
+        { name: 'Report a bug', icon: 'mdi-bug' },
+        { name: 'Github issue board', icon: 'mdi-github' },
+        { name: 'Stack overview', icon: 'mdi-stack-overflow' }
+      ],
+      nav_lists: [
+        { name: 'Getting Started', icon: 'mdi-vuetify' },
+        { name: 'Customization', icon: 'mdi-cogs' },
+        { name: 'Styles & animations', icon: 'mdi-palette' },
+        { name: 'UI Components', icon: 'mdi-view-dashboard' },
+        { name: 'Directives', icon: 'mdi-function' },
+        { name: 'Preminum themes', icon: 'mdi-vuetify' }
+      ]
+    };
+  }
+};
+</script>
